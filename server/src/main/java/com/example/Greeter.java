245219ad0acfb1edaff6bee166db9ca3874b0df3
Sergey <skonovalov@gmail.com>
2017-12-31 13:07:00 +0300

    Add JAVADOC to method

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -5,13 +5,14 @@ package com.example;
  */
 public class Greeter {
 
-  /**
-   * This is a constructor.
-   */
   public Greeter() {
 
   }
 
+  /**
+  * @param someone, name of person
+  * @return greeting string
+  */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
